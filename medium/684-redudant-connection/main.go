f02b7cedaab7dbdb3fed6673d72370a3f66aecaf
NamalSanjaya <namalsanjaya23@gmail.com>
2023-02-18 09:14:40 +0530

    Improve solution m-684

--- medium/684-redudant-connection/main.go
+++ medium/684-redudant-connection/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	// edges := [][]int{{1, 2}, {2, 3}, {3, 4}, {1, 4}, {1, 5}}
-	edges := [][]int{{1, 2}, {1, 3}, {2, 3}}
+	edges := [][]int{{1, 2}, {2, 3}, {3, 4}, {1, 4}, {1, 5}}
+	// edges := [][]int{{1, 2}, {1, 3}, {2, 3}}
 	fmt.Println(findRedundantConnection(edges))
 }
 
@@ -13,28 +13,36 @@ func findRedundantConnection(edges [][]int) []int {
 	for indx := range ufArray {
 		ufArray[indx] = indx + 1
 	}
-	var find func(x int) int
-	find = func(x int) int {
-		if ufArray[x-1] == x {
-			return x
-		}
-		return find(ufArray[x-1])
-	}
-	var unify func(x1, x2 int) bool
-	unify = func(x1, x2 int) bool {
-		p1 := find(x1)
-		p2 := find(x2)
-		if p1 == p2 {
-			return false
-		}
-		ufArray[p2-1] = p1
-		return true
-	}
 	lastEdge := []int{}
 	for _, edge := range edges {
-		if !unify(edge[0], edge[1]) {
+		if !unify(ufArray, edge[0], edge[1]) {
 			lastEdge = edge
 		}
 	}
 	return lastEdge
+}
+
+func find(ufArray []int, preNodes []int, x int) int {
+	if ufArray[x-1] == x {
+		pathCompression(ufArray, preNodes, x)
+		return x
+	}
+	preNodes = append(preNodes, x)
+	return find(ufArray, preNodes, ufArray[x-1])
+}
+
+func unify(ufArray []int, x1, x2 int) bool {
+	p1 := find(ufArray, []int{}, x1)
+	p2 := find(ufArray, []int{}, x2)
+	if p1 == p2 {
+		return false
+	}
+	ufArray[p2-1] = p1
+	return true
+}
+
+func pathCompression(ufArray []int, nodes []int, parent int) {
+	for _, node := range nodes {
+		ufArray[node-1] = parent
+	}
 }
