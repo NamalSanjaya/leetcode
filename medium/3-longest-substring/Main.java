2bd6accfa1965ec9c7afae663e19a9798bdf58e7
NamalSanjaya <namalsanjaya23@gmail.com>
2025-01-11 09:14:28 +0530

    Add solution to 3 longest substring problem

---
+++ medium/3-longest-substring/Main.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static int lengthOfLongestSubstring(String s) {
+        int maxLength = 0;
+
+        HashSet<Character> set = new HashSet<>();
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char currentChar = s.charAt(right);
+
+            while (set.contains(currentChar)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(currentChar);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb")); // Output: 3
+        System.out.println(lengthOfLongestSubstring("bbbbb"));    // Output: 1
+        System.out.println(lengthOfLongestSubstring("pwwkew"));   // Output: 3
+    }
+}
