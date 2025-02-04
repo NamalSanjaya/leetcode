b0875be675faba29bd569054fc7c03ad507dcc47
NamalSanjaya <namalsanjaya23@gmail.com>
2025-02-04 08:59:52 +0530

    Add comment to 2071 problem

--- hard/2071-max-task-can-assign/Main.java
+++ hard/2071-max-task-can-assign/Main.java
@@ -1,3 +1,5 @@
+// DID NOT PASS ALL TEST CASES. THIS CODE HAS SOME ISSUES.
+
 import java.util.*;
 
 public class MaxTasksAssign {
@@ -20,19 +22,18 @@ public class MaxTasksAssign {
     }
 
     private boolean canAssign(int k, int[] tasks, int[] workers, int pills, int strength) {
-        Deque<Integer> dq = new ArrayDeque<>();
-        int j = workers.length - k, usedPills = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int j = workers.length - 1, usedPills = 0;
 
-        for (int i = 0; i < k; i++) {
-            while (j < workers.length && workers[j] < tasks[i]) {
-                dq.offer(workers[j]);
-                j++;
+        for (int i = k - 1; i >= 0; i--) {
+            while (j >= workers.length - k && workers[j] >= tasks[i]) {
+                pq.offer(workers[j]);
+                j--;
             }
-            if (!dq.isEmpty() && usedPills < pills && dq.peek() + strength >= tasks[i]) {
-                dq.poll();
+            if (!pq.isEmpty()) {
+                pq.poll();
+            } else if (usedPills < pills) {
                 usedPills++;
-            } else if (j < workers.length && workers[j] >= tasks[i]) {
-                j++;
             } else {
                 return false;
             }
@@ -44,5 +45,6 @@ public class MaxTasksAssign {
         MaxTasksAssign mta = new MaxTasksAssign();
         System.out.println(mta.maxTaskAssign(new int[]{3,2,1}, new int[]{0,3,3}, 1, 1));
         System.out.println(mta.maxTaskAssign(new int[]{5,4}, new int[]{0,0,0}, 1, 5));
+        System.out.println(mta.maxTaskAssign(new int[]{5,9,8,5,9}, new int[]{1,6,4,2,6}, 1, 5));
     }
 }
