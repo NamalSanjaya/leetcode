0b487abf68dd18f33aca8e4b63a8b8a851457e8c
NamalSanjaya <namalsanjaya23@gmail.com>
2025-02-07 22:39:20 +0530

    Add solution to 3160 problem

---
+++ medium/3160-distince-color-balls/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class DistinctColors {
+    public static int[] countDistinctColors(int limit, int[][] queries) {
+        Map<Integer, Integer> ballColors = new HashMap<>();
+        Set<Integer> distinctColors = new HashSet<>();
+        int[] result = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int ball = queries[i][0];
+            int color = queries[i][1];
+
+            if (ballColors.containsKey(ball)) {
+                int prevColor = ballColors.get(ball);
+                distinctColors.remove(prevColor);
+            }
+
+            ballColors.put(ball, color);
+            distinctColors.add(color);
+
+            result[i] = distinctColors.size();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int limit = 4;
+        int[][] queries = { {1,4}, {2,5}, {1,3}, {3,4} };
+        System.out.println(Arrays.toString(countDistinctColors(limit, queries)));
+    }
+}
