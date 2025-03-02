c995a6eeb6a1625078c4a4dd9f75425de4c26398
NamalSanjaya <namalsanjaya23@gmail.com>
2025-03-02 19:13:02 +0530

    Add solution to 753 cracking safe problem

---
+++ hard/753-craking-safe/Main.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CrackingTheSafe {
+    public String crackSafe(int n, int k) {
+        StringBuilder result = new StringBuilder();
+        Set<String> visited = new HashSet<>();
+        String start = "0".repeat(n - 1);
+
+        dfs(start, k, visited, result);
+
+        result.append(start);
+        return result.toString();
+    }
+
+    private void dfs(String node, int k, Set<String> visited, StringBuilder result) {
+        for (int i = 0; i < k; i++) {
+            String newPassword = node + i;
+            if (!visited.contains(newPassword)) {
+                visited.add(newPassword);
+                dfs(newPassword.substring(1), k, visited, result);
+                result.append(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CrackingTheSafe solver = new CrackingTheSafe();
+        System.out.println(solver.crackSafe(1, 2));
+        System.out.println(solver.crackSafe(2, 2));
+    }
+}
