29c156af3db74ec4eb37707aa96137b408a3d280
NamalSanjaya <namalsanjaya23@gmail.com>
2025-03-13 23:50:46 +0530

    Rename to minZero method name

--- medium/3356-zero-arr-transform2/Main.java
+++ medium/3356-zero-arr-transform2/Main.java
@@ -1,9 +1,8 @@
 import java.util.Arrays;
 
 public class ZeroArrayTransformationII {
-    public static int minQueriesToZeroArray(int[] nums, int[][] queries) {
+    public static int minZeroArray(int[] nums, int[][] queries) {
         int n = nums.length;
-
         for (int k = 1; k <= queries.length; k++) {
             int[] temp = Arrays.copyOf(nums, n);
             boolean isZeroArray = applyQueries(temp, queries, k);
@@ -19,7 +18,6 @@ public class ZeroArrayTransformationII {
             int l = queries[i][0];
             int r = queries[i][1];
             int val = queries[i][2];
-
             for (int j = l; j <= r; j++) {
                 nums[j] = Math.max(0, nums[j] - val);
             }
@@ -34,10 +32,10 @@ public class ZeroArrayTransformationII {
     public static void main(String[] args) {
         int[] nums1 = {2, 0, 2};
         int[][] queries1 = {{0, 2, 1}, {0, 2, 1}, {1, 1, 3}};
-        System.out.println(minQueriesToZeroArray(nums1, queries1));
+        System.out.println(minZeroArray(nums1, queries1));
 
         int[] nums2 = {4, 3, 2, 1};
         int[][] queries2 = {{1, 3, 2}, {0, 2, 1}};
-        System.out.println(minQueriesToZeroArray(nums2, queries2));
+        System.out.println(minZeroArray(nums2, queries2));
     }
 }
