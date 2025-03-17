8f6ca4b26916105920b5114bb9cf17103f40f307
NamalSanjaya <namalsanjaya23@gmail.com>
2025-03-17 06:59:28 +0530

    Add test cases to 1546 max non overlapping sub array problem

--- medium/1546-max-subarrarys/Main.java
+++ medium/1546-max-subarrarys/Main.java
@@ -21,5 +21,14 @@ class Solution {
 
         return count;
     }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {1, 1, 1, 1, 1};
+        int target1 = 2;
+        System.out.println(solution.maxNonOverlapping(nums1, target1));
 
+        int[] nums2 = {-1, 3, 5, 1, 4, 2, -9};
+        int target2 = 6;
+        System.out.println(solution.maxNonOverlapping(nums2, target2));
+    }
 }
