71d44c9d7e5b4e4a3d14a37a0316d4745138448e
NamalSanjaya <namalsanjaya23@gmail.com>
2025-03-31 00:48:58 +0530

    Add test case to 11 medium problem

--- medium/11-container-with-most-water/Main.java
+++ medium/11-container-with-most-water/Main.java
@@ -17,4 +17,13 @@ class Solution {
         }
         return maxArea;
     }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int result = solution.maxArea(height);
+
+        System.out.println(result);
+    }
 }
