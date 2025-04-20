7c542617e279be5ac54a4428dd8fd8fe41494f89
NamalSanjaya <namalsanjaya23@gmail.com>
2025-04-21 00:33:39 +0530

    Remove comments

--- medium/5-longest-palindromic-substring/Main.java
+++ medium/5-longest-palindromic-substring/Main.java
@@ -5,8 +5,8 @@ public class Solution {
         int start = 0, end = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expandFromCenter(s, i, i);       // Odd-length palindromes
-            int len2 = expandFromCenter(s, i, i + 1);   // Even-length palindromes
+            int len1 = expandFromCenter(s, i, i);
+            int len2 = expandFromCenter(s, i, i + 1);
             int len = Math.max(len1, len2);
 
             if (len > end - start) {
